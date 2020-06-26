696de3729f3ed7606ce05e6232fad50ed2bfddc1
cdelattre03 <cyril.delattre02@gmail.com>
2020-06-26 14:57:40 +0200

    first commit

---
+++ systeme de login/config.php
@@ -0,0 +1,16 @@
+<?php
+// Informations d'identification
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'rooot');
+define('DB_PASSWORD', 'rooot');
+define('DB_NAME', 'registration');
+ 
+// Connexion à la base de données MySQL 
+$conn = mysqli_connect("127.0.0.1", "rooot", "rooot", "registration");
+ 
+// Vérifier la connexion
+if($conn == false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+?>
+
